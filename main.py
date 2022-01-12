4dd68f3996189c9016a11174e041f28f285585ff
Robert Eckwright <reckwright98@gmail.com>
2022-01-11 22:14:54 -0800

    added function to produce rooms

--- main.py
+++ main.py
@@ -1,10 +1,31 @@
 #!usr/bin/env python3
+"""main file for a basic python roguelike"""
 
 import curses
 from curses import wrapper
 
 
+def generate_room(width: int, height: int) -> list:
+    """Generate a rectangular room with given width and height"""
+    room = []
+
+    for i in range(height):
+        string = ""
+        for j in range(width):
+            if i in (0, width - 1):
+                string += "-"
+            else:
+                if j in (0, width - 1):
+                    string += "|"
+                else:
+                    string += "."
+        room.append(string)
+    return room
+
+
 def main(stdscr):
+    """main function"""
+
     height, width = stdscr.getmaxyx()
     player_y = height//2
     player_x = width//2
@@ -14,6 +35,9 @@ def main(stdscr):
 
     while True:
         stdscr.clear()
+        new_room = generate_room(10, 10)
+        for i, level in enumerate(new_room):
+            stdscr.addstr(i + 1, 1, level)
         stdscr.addstr(player_y, player_x, "@")
         stdscr.refresh()
 
